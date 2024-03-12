6f38c31606d28de09e65aa47f2b83ef5f88f9a85
Vinit Dharaiya <132012257+vinitD124@users.noreply.github.com>
2024-03-13 00:56:25 +0530

    Update index.js
    four

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const dbConnect = require("../Backend/confg/database")
 const routes = require("../Backend/routes/routes")
 const cors = require("cors");
 const app = express()
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: ["https://vercel-crud-jvik-fronted.vercel.app/"],methods:["POST","GET","PUT","DELETE"],credentials:true }));
 app.use(express.json())
 require("dotenv").config()
 app.use("/api/v1", routes)
